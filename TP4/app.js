92c22950a5daec83b9c983719ef19f504d25f718
alangonza <alanalcos@gmail.com>
2024-10-31 10:55:54 -0300

    se subio el tp4

---
+++ TP4/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+// Crear una instancia de Express
+const app = express();
+// Configurar el puerto en el que el servidor escuchará
+const PORT = 3000;
+
+// Ruta básica
+app.get("/", (req, res) => {
+  res.send("Hola, mundo");
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
