e2c2e3c96efb51d443598683b2bb4e4ae81f2de3
alangonza <alanalcos@gmail.com>
2024-11-03 05:44:15 -0300

    Eliminación masiva de archivos innecesarios

--- TP4/project-root/repository/superheroesDataSource.mjs
+++
@@ -1,8 +0,0 @@
-// repository/superheroesDataSource.mjs
-
-export default class SuperheroesDataSource {
-  // Método abstracto para obtener todos los superhéroes
-  obtenerTodos() {
-    throw new Error("Este método debe ser implementado por la subclase");
-  }
-}
